365cf7cf818fedd5364c204700f5813524fe386b
davidovits1 <david.o3111@gmail.com>
2023-08-07 17:13:00 +0300

    work for most red and green lights

--- code-base/run_attention.py
+++ code-base/run_attention.py
@@ -10,7 +10,8 @@ from matplotlib.axes import Axes
 
 # Internal imports... Should not fail
 from consts import IMAG_PATH, JSON_PATH, NAME, SEQ_IMAG, X, Y, COLOR, RED, GRN, DATA_DIR, TFLS_CSV, CSV_OUTPUT, \
-    SEQ, CROP_DIR, CROP_CSV_NAME, ATTENTION_RESULT, ATTENTION_CSV_NAME, ZOOM, RELEVANT_IMAGE_PATH, COL, ATTENTION_PATH
+    SEQ, CROP_DIR, CROP_CSV_NAME, ATTENTION_RESULT, ATTENTION_CSV_NAME, ZOOM, RELEVANT_IMAGE_PATH, COL, ATTENTION_PATH, \
+    CSV_INPUT
 from misc_goodies import show_image_and_gt
 from data_utils import get_images_metadata
 from crops_creator import create_crops
@@ -21,7 +22,7 @@ from pandas import DataFrame, Series
 import numpy as np
 from scipy import signal as sg
 import scipy.ndimage as ndimage
-from scipy.ndimage import maximum_filter
+from scipy.ndimage import maximum_filter, convolve
 from PIL import Image
 import matplotlib.pyplot as plt
 
@@ -29,28 +30,95 @@ import matplotlib.pyplot as plt
 def find_tfl_lights(c_image: np.ndarray, **kwargs) -> Dict[str, Any]:
     """
     Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement.
-
     :param c_image: a H*W*3 RGB image of dtype np.uint8 (RGB, 0-255).
     :param kwargs: Whatever you want.
     :return: Dictionary with at least the following keys: 'x', 'y', 'col', each containing a list (same lengths).
     # Note there are no explicit strings in the code-base. ALWAYS USE A CONSTANT VARIABLE INSTEAD!.
     """
+    # Get the image dimensions and the number of channels (3 for RGB)
+    height, width, channels = c_image.shape
+
+    # template = Image.open("red_light3.png")
+    # resized_temp = template.resize((3, 3))
+    # temp_np = np.array(resized_temp) / 255.0
+    # temp_np = temp_np[:, :, 0]
+    # k_red = temp_np / np.sum(temp_np)
+    # Define the convolution kernel to detect red regions
+    kernel = np.array([[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]])
+    #kernel = np.array([[1 / 16, 2 / 16, 1 / 16], [2 / 16, 4 / 16, 2 / 16], [1 / 16, 2 / 16, 1 / 16]])
+    # Apply the convolution operation to get a redness score for each pixel
+    redness_score_green = convolve(c_image[:, :, 1], kernel)
+    redness_score_red = convolve(c_image[:, :, 0], kernel)
+    redness_score_blue = convolve(c_image[:, :, 2], kernel)
+    #empty lists to store the x and y coordinates of the red and green regions
+    x_red: List[float] = []
+    y_red: List[float] = []
+    x_green: List[float] = []
+    y_green: List[float] = []
+    threshold_red = np.float32(253/255)
+    threshold_green = np.float32(220/255)
+
+    list_point =[]
+    #max_rsr = maximum_filter(redness_score_red, 150, mode='constant')
+    #max_rsg = maximum_filter(redness_score_green, 150, mode='constant')
+    # Loop through each pixel and mark the red regions with an "X" mark
+    for y in range(height):
+        for x in range(width):
+            # Check if the redness score is above a threshold to consider it as a red region
+            rsg = redness_score_green[y, x]
+            rsr = redness_score_red[y, x]
+            rsb = redness_score_blue[y, x]
+            if rsg > (rsb + rsr) * 0.67 and rsg > threshold_green:
+                # x_green.append(x)
+                # y_green.append(y)
+                for i in range(len(list_point)):
+                    if x - 15 <= list_point[i][0] <= x + 15 and y - 15 <= list_point[i][1] <= y + 15:
+                        if list_point[i][2] < rsg:
+                            list_point[i][0] = x
+                            list_point[i][1] = y
+                            list_point[i][2] = rsg
+                            break
+                        else:
+                            break
+                else:
+                    list_point.append([x, y, rsg, 'g'])
+            elif rsr > (rsb + rsg) * 0.65 and rsr > threshold_red:
+                # x_red.append(x)
+                # y_red.append(y)
+                for i in range(len(list_point)):
+                    if x - 15 <= list_point[i][0] <= x + 15 and y - 15 <= list_point[i][1] <= y + 15:
+                        if list_point[i][2] < rsr:
+                            list_point[i][0] = x
+                            list_point[i][1] = y
+                            list_point[i][2] = rsr
+                            break
+                        else:
+                            break
+                else:
+                    list_point.append([x, y, rsr, 'r'])
 
     # Okay... Here's an example of what this function should return. You will write your own of course
-    x_red: List[float] = (np.arange(-100, 100, 20) + c_image.shape[1] / 2).tolist()
-    y_red: List[float] = [c_image.shape[0] / 2 - 120] * len(x_red)
-    x_green: List[float] = x_red
-    y_green: List[float] = [c_image.shape[0] / 2 - 100] * len(x_red)
+   # x_red: List[float] = (np.arange(-100, 100, 20) + c_image.shape[1] / 2).tolist()
+   # y_red: List[float] = [c_image.shape[0] / 2 - 120] * len(x_red)
+   # x_green: List[float] = x_red
+   # y_green: List[float] = [c_image.shape[0] / 2 - 100] * len(x_red)
 
+    for [x, y, _, ch] in list_point:
+        if ch == 'g':
+            x_green.append(x)
+            y_green.append(y)
+        else:
+            x_red.append(x)
+            y_red.append(y)
+    print("green : " + str(len(x_green)) + " red : " + str(len(x_red)))
     if kwargs.get('debug', False):
         # This is here just so you know you can do it... Look at parse_arguments() for details
         if np.random.rand() > kwargs.get('some_threshold', 0) / 45:
             print("You're lucky, aren't you???")
-
-    return {X: x_red + x_green,
-            Y: y_red + y_green,
-            COLOR: [RED] * len(x_red) + [GRN] * len(x_green),
-            }
+        return {X: x_red + x_green,
+                 Y: y_red + y_green,
+                 COLOR: [RED] * len(x_red) + [GRN] * len(x_green),
+                }
 
 
 def test_find_tfl_lights(row: Series, args: Namespace) -> DataFrame:
@@ -60,7 +128,6 @@ def test_find_tfl_lights(row: Series, args: Namespace) -> DataFrame:
     image_path: str = row[IMAG_PATH]
     json_path: str = row[JSON_PATH]
     image: np.ndarray = np.array(Image.open(image_path), dtype=np.float32) / 255
-
     if args.debug and json_path is not None:
         # This code-base demonstrates the fact you can read the bounding polygons from the json files
         # Then plot them on the image. Try it if you think you want to. Not a must...
@@ -70,22 +137,18 @@ def test_find_tfl_lights(row: Series, args: Namespace) -> DataFrame:
         ax: Optional[Axes] = show_image_and_gt(image, objects, f"{row[SEQ_IMAG]}: {row[NAME]} GT")
     else:
         ax = None
-
     # In case you want, you can pass any parameter to find_tfl_lights, because it uses **kwargs
     attention_dict: Dict[str, Any] = find_tfl_lights(image, some_threshold=42, debug=args.debug)
     attention: DataFrame = pd.DataFrame(attention_dict)
-
     # Copy all image metadata from the row into the results, so we can track it later
     for k, v in row.items():
         attention[k] = v
-
     tfl_x: np.ndarray = attention[X].values
     tfl_y: np.ndarray = attention[Y].values
     color: np.ndarray = attention[COLOR].values
     is_red = color == RED
-
-    print(f"Image: {image_path}, {is_red.sum()} reds, {len(is_red) - is_red.sum()} greens..")
-
+    is_green = color == GRN
+    print(f"Image: {image_path}, {len(is_red)} reds, {len(is_green)} greens..")
     if args.debug:
         # And here are some tips & tricks regarding matplotlib
         # They will look like pictures if you use jupyter, and like magic if you use pycharm!
@@ -107,7 +170,6 @@ def test_find_tfl_lights(row: Series, args: Namespace) -> DataFrame:
         plt.imshow(hp_result)
         plt.title('Some useless image for you')
         plt.suptitle("When you zoom on one, the other zooms too :-)")
-
     return attention
 
 
@@ -127,8 +189,7 @@ def prepare_list(in_csv_file: Path, args: Namespace) -> DataFrame:
     csv_list: DataFrame = get_images_metadata(in_csv_file,
                                               max_count=args.count,
                                               take_specific=args.image)
-    return pd.concat([pd.DataFrame(columns=CSV_OUTPUT), csv_list], ignore_index=True)
-
+    return pd.concat([pd.DataFrame(columns=CSV_INPUT), csv_list], ignore_index=True)
 
 def run_on_list(meta_table: pd.DataFrame, func: callable, args: Namespace) -> pd.DataFrame:
     """
